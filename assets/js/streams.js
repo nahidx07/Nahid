907ed1e66a14e90bb3a98ea930fd2e4e3e41940e
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-16 15:42:45 +0600

    Update streams.js

--- assets/js/streams.js
+++ assets/js/streams.js
@@ -2,17 +2,16 @@
 import { collection, getDocs } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const container = document.getElementById("streams");
+const box = document.getElementById("streams");
 const snap = await getDocs(collection(db, "streams"));
 
-snap.forEach(doc => {
-  const s = doc.data();
-  container.innerHTML += `
-    <div class="card" onclick="location.href='player.html?id=${doc.id}'">
+snap.forEach(d => {
+  const s = d.data();
+  box.innerHTML += `
+    <div class="card" onclick="location.href='player.html?id=${d.id}'">
       <img src="${s.thumbnail}">
       <h4>${s.title}</h4>
       <span class="live">LIVE</span>
-    </div>
-  `;
+    </div>`;
 });
 </script>
