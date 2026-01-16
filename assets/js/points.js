3b628acd8366096207f2aac9b1f7ce5896e0866c
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-16 15:44:06 +0600

    Update points.js

--- assets/js/points.js
+++ assets/js/points.js
@@ -3,13 +3,12 @@ import { doc, updateDoc, increment } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let sec = 0;
-
 setInterval(async () => {
   sec++;
   if (sec % 60 === 0) {
-    await updateDoc(doc(db, "users", telegramUser.userId), {
+    await updateDoc(doc(db,"users",telegramUser.userId),{
       points: increment(10)
     });
   }
-}, 1000);
+},1000);
 </script>
