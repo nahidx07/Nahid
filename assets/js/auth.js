df7e85e3e0c09003c4e35bf0bc2ed7676f3b7bec
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-16 15:34:53 +0600

    Create auth.js

---
+++ assets/js/auth.js
@@ -0,0 +1,23 @@
+<script type="module">
+import { doc, getDoc, setDoc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+async function createUser() {
+  const ref = doc(db, "users", telegramUser.userId);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    await setDoc(ref, {
+      userId: telegramUser.userId,
+      username: telegramUser.username,
+      name: telegramUser.name,
+      photo: telegramUser.photo,
+      points: 0,
+      referralCode: "REF" + telegramUser.userId,
+      createdAt: Date.now()
+    });
+  }
+}
+
+createUser();
+</script>
