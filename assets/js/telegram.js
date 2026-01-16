3d9e10fcb4a00ab6ca61d2f7bd18df37675f4f6e
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-16 15:41:10 +0600

    Update telegram.js

--- assets/js/telegram.js
+++ assets/js/telegram.js
@@ -2,12 +2,11 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
 
-const user = tg.initDataUnsafe.user;
-
+const u = tg.initDataUnsafe.user;
 window.telegramUser = {
-  userId: user.id.toString(),
-  username: user.username || "",
-  name: `${user.first_name || ""} ${user.last_name || ""}`,
-  photo: user.photo_url || ""
+  userId: u.id.toString(),
+  username: u.username || "",
+  name: `${u.first_name || ""} ${u.last_name || ""}`,
+  photo: u.photo_url || ""
 };
 </script>
